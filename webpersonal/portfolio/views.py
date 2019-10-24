4dd09dac140a74b4f7a48bff533dea8e0741f72a
chrisrm86 <christianrmoran86@gmail.com>
2019-10-24 15:29:40 -0300

    improvements on repository 1

--- webpersonal/portfolio/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Project
-
-# Create your views here.
-
-def portfolio(request):
-    project = Project.objects.all()
-	return render(request,"portfolio/portfolio.html", {'projects':projects})
