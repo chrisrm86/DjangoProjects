4cd0a5364e993b37db49c93c03d29de6d0f5ef95
chrisrm86 <christianrmoran86@gmail.com>
2019-10-24 00:06:32 -0300

    setting up MVT

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -15,12 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
+from core import views as core_views
+from portfolio import views as portfolio_views
+
+from django.conf import settings
 
 urlpatterns = [
-	path('', views.home, name="home"),
-	path('about/', views.about, name="about"),
-	path('portfolio/', views.portfolio, name="portfolio"),
-	path('contact/', views.contact, name="contact"),
+	path('', core_views.home, name="home"),
+	path('about/', core_views.about, name="about"),
+	path('portfolio/', portfolio_views.portfolio, name="portfolio"),
+	path('contact/', core_views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+	from django.conf.urls.static import static
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
