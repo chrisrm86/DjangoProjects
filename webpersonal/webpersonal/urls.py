6e9745221a11d36356dd9664a4f84aab023ec262
chrisrm86 <christianrmoran86@gmail.com>
2020-02-24 11:53:46 -0300

    added template tag  url

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
+    path('home/', views.home, name="home"),
+    path('about-me/', views.about, name="about"),
     path('portfolio/', views.portfolio, name='portfolio'),
     path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
