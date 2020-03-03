de218f6c32da7b18e9f5ec6b386002308154e894
chrisrm86 <christianrmoran86@gmail.com>
2020-03-03 00:57:53 -0300

    created portfolio app, model, and super user to manage project

---
+++ webpersonal/portfolio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
