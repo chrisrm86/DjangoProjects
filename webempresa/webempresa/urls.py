00cd5ee1bced3581cea1cd9b0bd829fbef9ae4b6
chrisrm86 <christianrmoran86@gmail.com>
2020-03-08 11:21:43 -0300

    created global url for core views

--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -14,16 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('services/', views.services, name="services"),
-    path('store/', views.store, name="store"),
-    path('contact/', views.contact, name="contact"),
-    path('blog/', views.blog, name="blog"),
-    path('sample/', views.sample, name="sample"),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
