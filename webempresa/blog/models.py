6a14d27f69bd6a7c319befeb352877e1959e1f3c
chrisrm86 <christianrmoran86@gmail.com>
2020-03-21 15:11:00 -0300

    modificated views of app blog

--- webempresa/blog/models.py
+++ webempresa/blog/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
     published = models.DateTimeField(default=now, verbose_name='Fecha de publicacion')
     image = models.ImageField(upload_to='blog', null=True, blank=True, verbose_name='Imagen')
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Autor')
-    categories = models.ManyToManyField(Category, verbose_name='Categorias')
+    categories = models.ManyToManyField(Category, verbose_name='Categorias', related_name='get_posts')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creacion')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edicion')
 
--- webempresa/blog/views.py
+++ webempresa/blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 
 def blog(request):
     posts = Post.objects.all()
     return render(request, "blog/blog.html", {'posts':posts})
 
-# Create your views here.
+def category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    #category = Category.objects.get(id=category_id)
+    posts = Post.objects.filter(categories=category)
+    return render(request, "blog/category.html",
+                  {'category':category, 'posts':posts})
