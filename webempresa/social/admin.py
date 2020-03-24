18c621316b2fa6290364b7b6e71e7263bc535dfa
chrisrm86 <christianrmoran86@gmail.com>
2020-03-23 23:50:25 -0300

    Limited permissions to users

--- webempresa/social/admin.py
+++ webempresa/social/admin.py
@@ -3,6 +3,12 @@ from .models import Link
 
 # Register your models here.
 class LinkAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ('created','updated')
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name="Personal").exists():
+            return ('key', 'name')
+        else:
+            return ('created','updated')
 
 admin.site.register(Link, LinkAdmin)
