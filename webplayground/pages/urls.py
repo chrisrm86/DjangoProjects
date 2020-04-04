c823206eb32c5d7bd53b273b0590848a61ae2ce4
chrisrm86 <christianrmoran86@gmail.com>
2020-04-04 20:02:28 -0300

    Implemented ListView and DetailView

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from .views import PageListView, PageDetailView
 
 urlpatterns = [
-    path('', views.pages, name='pages'),
-    path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
+    path('', PageListView.as_view(), name='pages'),
+    path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
 ]
--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Page
 
 # Create your views here.
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
 
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
+class PageListView(ListView):
+    model = Page
+
+
+class PageDetailView(DetailView):
+    model = Page 
