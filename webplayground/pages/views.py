073fb4103d4d03be10cc94479e099c9785511323
chrisrm86 <christianrmoran86@gmail.com>
2020-04-04 21:54:39 -0300

    Implemented CBV ListView and DetailView

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Page
